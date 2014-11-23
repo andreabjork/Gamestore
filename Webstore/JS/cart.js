e1f263a5d694adada262c462728fb957bd197149
leojo <leojohannsson91@gmail.com>
2014-11-23 15:01:39 +0000

    cart prototype

--- Webstore/JS/cart.js
+++ Webstore/JS/cart.js
@@ -1,21 +1,7 @@
-function getUrlParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
-}   
-
 $('.cartBtn').click(function(e){
 	var id = $(e.target).attr('prod_id');
 	var type = $(e.target).attr('class')[1];
-	var username = getUrlParameter('user');
+	var username = $(e.target).attr('user');
 	$.ajax({
 		url: 'functions/cart_actions.php',
 		data: {action: type, user: username, prod_id: id},
--- Webstore/views/cart_contents.php
+++ Webstore/views/cart_contents.php
@@ -2,6 +2,31 @@
 		<?php
 			$results = $cur->query("SELECT * FROM ShoppingCarts WHERE user=$user");
 			$data = $results->fetchAll();
-			
 		?>
-		<script type="text/javascript" src="JS/cart.js"></script>
+		<h1>Your cart:</h1>
+		<?php
+			foreach ($data as $row) {
+				$id = $row['prod_id'];
+				$qty = $row['quantity'];
+				$result = $cur->query("SELECT * FROM Products WHERE id=$id");
+				$info = $result->fetchAll(); 
+				$title = $info['name'];
+				$img = $info['img_src'];
+				$price = $info['price'];
+				$totPrice = $price*$qty;
+				echo "<div id=$id>";
+					echo "<div class='prodInfo'>";
+						echo "<h1>$title</h1>";
+						echo "<img src='$img' />";
+					echo "</div>";
+					echo "<span class='price'>$totPrice</span>";
+					echo "<div class='incdec'>";
+						echo"<input type='button' class='cartBtn increment' prod_id=$id user='$user'>";
+						echo"<span class='quantity'>$qty</span>";
+						echo"<input type='button' class='cartBtn decrement' prod_id=$id user='$user'>";
+					echo "</div>";
+					echo "<input type='button' class='cartBtn remove' prod_id=$id user='$user'>";
+				echo "</div>";
+			}
+		?>
+		<!--<script type="text/javascript" src="JS/cart.js"></script>-->
