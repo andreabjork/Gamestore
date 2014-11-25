4d46668796a7794fe516994f86716c084748e8e6
leojo <leojohannsson91@gmail.com>
2014-11-25 17:25:09 +0000

    resize komið í gagnið!

--- Webstore/JS/justify_height.js
+++ Webstore/JS/justify_height.js
@@ -1,3 +1,30 @@
-var height = $("#body").height();
-console.log("this is the height "+height);
+$(window).on("load resize",scaleHeight);
+
+function scaleHeight(){
+	console.log("running scaleHeight");
+	$(".container").height("auto");
+	
+	var Dheight = $(document).height();
+	var Wheight = $(window).height();
+	
+	console.log("document: "+Dheight);
+	console.log("window:   "+Wheight);
+	
+	var height = Math.max(Dheight,Wheight);
+	
+	console.log("height:   "+height);
+	
+	var headerHeight = 180;
+	var footerHeight = 100;
+	var contentHeight = $(".container").height();
+	
+	console.log("current height of content "+contentHeight);
+	
+	var totalHeight = headerHeight + footerHeight + contentHeight;
+
+	var availableHeight = height-(headerHeight+footerHeight);
+	console.log("Available height for content is "+availableHeight);
+	$(".container").height(availableHeight);
+	console.log("Height should be changed now");
+}
 
