5f4dd9dfe05b703f71d0b13f00a4e64b062b7f2b
andreabjork <andreabjork43@gmail.com>
2014-11-25 20:32:54 +0000

    some uncommitted

--- Webstore/JS/cart.js
+++ Webstore/JS/cart.js
@@ -2,6 +2,9 @@ $('.cartBtn').click(function(e){
 	var id = $(e.target).attr('prod_id');
 	var type = $(e.target).attr('class').split(" ");
 	var username = $(e.target).attr('user');
+	var $thisBtn = $(this);
+	console.log('what is this?');
+	console.log($thisBtn);
 	console.log("this is the product id "+id);
 	console.log('this is the extra class '+type[1]);
 	console.log(typeof type);
@@ -13,48 +16,63 @@ $('.cartBtn').click(function(e){
 		type: 'post',
 		success: function(output) {
 			console.log(output);
-			//updateCart(type[1], id);
+			console.log('did we get in here?');
+			updateCart(type[1], $thisBtn);
+			console.log('get past this?');
 		}
 	});
 	
 });
 
-function updateCart(type,id){
+
+
+function updateCart(type, btn){
+	console.log("updating Cart....");
 	switch(type){
 		case 'increment':
-			increment(id);
+			increment(btn);
 			break;
 		case 'decrement':
-			decrement(id);
+			decrement(btn);
 			break;
 		case 'remove':
-			remove(id);
+			remove(btn);
 			break;
 		case 'add':
-			add(id);
+			add(btn);
 			break;
 		default:
 			break;
 	}
 }
 
-function increment(id) {
-	var selector = '#'+id+
-	$(divID+'.incdec')[0].childNode;
+function increment(btn) {
+	var spanEle = btn.parent().children();
+	var quantity = spanEle[1].innerHTML;
+	spanEle[1].innerHTML = parseInt(quantity)+1;
+	if(parseInt(quantity)===1) {
+		$(spanEle[2]).css("display", "block");
+	}
 }
 
-function decrement(id){
-	var divId = "#"+id;
-	
+function decrement(btn){
+	var spanEle = btn.parent().children();
+	var quantity = spanEle[1].innerHTML;
+	if(parseInt(quantity)>1) {
+		spanEle[1].innerHTML = parseInt(quantity)-1;
+	}
+	if(parseInt(quantity)<=2) {	
+		btn.css("display", "none");
+	}
+
 }
 
 function add(id){
-	var divId = "#"+id;
-	
+	return "";
 }
 
-function remove(id){
-	var divId = "#"+id;
+function remove(btn){
+	btn.parent().remove();
 	
 }
 
