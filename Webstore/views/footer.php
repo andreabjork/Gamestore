8eb809131d56ce0f64ff09b6316aeb873576329e
leojo <leojohannsson91@gmail.com>
2014-11-29 22:42:38 +0000

    footer fix

--- Webstore/views/footer.php
+++ Webstore/views/footer.php
@@ -6,8 +6,8 @@
 					<li id="email">boardgamer@boardgamer.com</li>
 				</ul>
 			</section>
-			<section class="right">
-				<ul id="hours"> Opening hours:
+			<section class="right">Opening hours:
+				<ul id="hours">
 					<li id="monFri">Mon-Fri: 10AM-7PM</li>
 					<li id="sat">Sat: 12AM-5PM</li>
 					<li id="sun">Sun: Closed.</li>
